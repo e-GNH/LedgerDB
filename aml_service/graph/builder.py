70b0d41c61502d6ad665f6d80510d4e7f1a27384
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-06-07 12:59:19 +0300

    cycle detection

--- aml_service/graph/builder.py
+++ aml_service/graph/builder.py
@@ -3,7 +3,8 @@ import networkx as nx
 class TransactionsGraph:
     def __init__(self):
         self.graph = nx.MultiDiGraph() ## Multi Directed Graph to allow multiple transactions between the same accounts
-    
+        self.LOOP_CUTOFF = 4
+        
     def add_transaction(self, from_account, to_account, amount, timestamp):
         self.graph.add_edge(from_account, to_account, amount=amount, timestamp=timestamp)
         
@@ -27,3 +28,37 @@ class TransactionsGraph:
     
     def get_output_money(self, account):
         return sum(data['amount'] for _, _, data in self.graph.out_edges(account, data=True))
+    
+    # TODO implement own version of all simple paths with pruning for timestamps to avoid generating all paths and then filtering them, which can be expensive
+    def get_money_cycled(self, account):
+                
+        loops_total_received = 0
+        remaining_capacity = {}
+        for u, v, key, data in self.graph.edges(keys=True, data=True):
+            remaining_capacity[(u, v, key)] = data['amount']
+            
+        for successor in set(self.graph.successors(account)): ## check each unique successor to avoid redundant path calculations
+            if nx.has_path(self.graph,successor, account): ## check if there is a path back to the original account
+                simple_paths = nx.all_simple_edge_paths(self.graph, source=successor, target=account, cutoff=self.LOOP_CUTOFF)
+                successor_min_amount = sum(data['amount'] for _, dest, data in self.graph.out_edges(account, data=True) if dest == successor)
+                ## This will catch false positives where there are multiple transactions from account to successor
+                # but only one of them is part of a loop. 
+                # but it is okay to have false positives here because we care more about recall than precision in AML
+                # Case: A->B 1000 B->A 1500 A->B 500, this will return 1500 as amount cycled back 
+                # but it is better than missing other cases where A->B 1000 A->B 500 B->A 1500 which needs 1500 to be returned
+                successor_timestamp = min(data['timestamp'] for _, dest, data in self.graph.out_edges(account, data=True) if dest == successor)
+                for path in simple_paths:
+                    min_amount = successor_min_amount ## initialize min_amount with the amount of the first transaction from account to successor, as this is the maximum amount that can be cycled back through this path
+                    timestamp = successor_timestamp ## initialize timestamp with the timestamp of the first transaction from account to successor
+                    for u, v, key in path:
+                        edge_data = self.graph.get_edge_data(u, v, key=key)
+                        if edge_data['timestamp'] >= timestamp:
+                            min_amount = min(min_amount, edge_data['amount'])
+                            timestamp = edge_data['timestamp']
+                        else:
+                            min_amount = 0
+                            break ## if we encounter an edge with timestamp older than the initial transaction, we can stop checking this path as it won't contribute to the loop amount
+                    loops_total_received += min_amount
+        
+        return loops_total_received
+                
--- aml_service/levels/level_graph.py
+++ aml_service/levels/level_graph.py
@@ -5,3 +5,10 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from graph.builder import TransactionsGraph
 
 gf = TransactionsGraph()
+
+gf.add_transaction('A', 'B', 1000, 1)
+gf.add_transaction('B', 'C', 800, 2)
+gf.add_transaction('B', 'C', 100, 2)
+gf.add_transaction('C', 'A', 100, 3)
+gf.add_transaction('C', 'A', 100, 3)
+print(gf.get_money_cycled('A')) ## should return 1000
