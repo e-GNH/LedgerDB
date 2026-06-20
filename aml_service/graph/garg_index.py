b48a5da36311c09673cd63f0594ee6cd6d20793e
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-06-20 14:26:41 +0300

    added louvain communities implementation

--- aml_service/graph/garg_index.py
+++ aml_service/graph/garg_index.py
@@ -1,5 +1,180 @@
 import networkx as nx
 import numpy as np
+
+class LouvainCommunities():
+    def __init__(self, graph, resolution = 1, use_weight = False):
+        self.graph = graph
+        self.resolution = resolution ## For controlling size of communities
+        self.mapping = dict()
+        i = 0
+        for node in graph:
+            self.mapping[i] = node
+            i += 1
+        
+        weight = 'weight' if use_weight else None
+        self.adjacency_matrix = nx.adjacency_matrix(graph, weight=weight).toarray()
+        self.cached_modularities = dict()
+    
+    def _compute_delta_modularity(sef):
+        #TODO: Implement and use instead of caching
+        return 0
+    
+    def _compute_modularity(self, communities, adjacency_matrix):
+        total_modularity = 0
+        
+        # m = sum of edges
+        m = np.sum(adjacency_matrix) / 2 #divide by 2 since undirected graph
+        if m == 0:
+            print("edges weigth in graphs = 0")
+            return 0
+        for community in communities:
+            community_modularity = 0
+            if community in self.cached_modularities:
+                community_modularity = self.cached_modularities[community]
+            else:
+                for node_i in community:
+                    for node_j in community:
+                        community_modularity += adjacency_matrix[node_i][node_j]
+                
+                community_modularity /= (2 * m)
+                
+                subtraction_term = 0
+                for node in community:
+                    subtraction_term += np.sum(adjacency_matrix[node])
+                    
+                subtraction_term /= (2 * m)
+                subtraction_term = subtraction_term ** 2
+                
+                community_modularity = community_modularity - subtraction_term * self.resolution
+                self.cached_modularities[frozenset(community)] = community_modularity
+                
+            total_modularity += community_modularity
+        return total_modularity
+
+    def _move_nodes(self, communities, adjacency_matrix):
+
+        old_modularity = 0
+        curr_modularity = 0        
+        initial_iteration = True
+        curr_modularity = self._compute_modularity(communities, adjacency_matrix)
+        while curr_modularity > old_modularity or initial_iteration:
+            initial_iteration = False
+            old_modularity = curr_modularity
+            
+            ## Move nodes ## loop by order of communities
+            visited = set()
+            for i in range(len(communities)):
+                community = communities[i]
+                for node_index in community:
+                    if node_index in visited:
+                        continue
+                    visited.add(node_index)
+                    ## try removing it
+                    best_idx = -1
+                    best_modularity = curr_modularity
+                    community_set = set(communities[i])
+                    community_set.remove(node_index)
+                    communities[i] = frozenset(community_set)
+                    for j in range(len(communities)):
+                        if i == j:
+                            continue
+                        ## try adding it
+                        community_j = communities[j]
+                        comm_j_set = set(community_j)
+                        comm_j_set.add(node_index)
+                        communities[j] = frozenset(comm_j_set)
+                        new_modularity = self._compute_modularity(communities, adjacency_matrix)
+                        if new_modularity > best_modularity:
+                            best_idx = j
+                            best_modularity = new_modularity
+                        communities[j] = community_j
+
+                    if best_modularity - curr_modularity > 1e-10:
+                        community_dst = communities[best_idx]
+                        comm_dst_set = set(community_dst)
+                        comm_dst_set.add(node_index)
+                        communities[best_idx] = frozenset(comm_dst_set)
+                        curr_modularity = best_modularity
+                    else:
+                        communitiy_orig = communities[i]
+                        community_orig_set = set(communitiy_orig)
+                        community_orig_set.add(node_index)
+                        communities[i] = frozenset(community_orig_set)
+                        
+            communities = [community for community in communities if len(community)]         
+
+        return communities            
+            
+    def _aggregate_graph(self, communities, adjacency_matrix):
+        n = len(communities)
+        new_adjacency_matrix = np.zeros((n, n))
+        for i in range(n):
+            ## add self loops
+            for node_i in communities[i]:
+                for node_j in communities[i]:
+                    new_adjacency_matrix[i, i] += adjacency_matrix[node_i, node_j]
+                    
+            for j in range(i + 1, n):
+                for node_i in communities[i]:
+                    for node_j in communities[j]:
+                        new_adjacency_matrix[i, j] += adjacency_matrix[node_i, node_j]
+                        new_adjacency_matrix[j, i] += adjacency_matrix[node_i, node_j]
+        return new_adjacency_matrix
+    
+    def _singleton_partition(self, mapping):
+        ## Initially one node per community
+        communities = list()
+        for node_index in mapping:
+            communities.append(frozenset([node_index]))
+        return communities
+    
+    def _flatten_clusters(self, new_communities, mappings):
+        levels = len(mappings)
+        finalized_clusters = list()
+        for community_idx in range(len(new_communities)):
+            curr_level_community = new_communities[community_idx]
+
+            for level in range(levels):
+                higher_community = list()
+                curr_level = levels - 1 - level
+                curr_mapping = mappings[curr_level]
+
+                for item in curr_level_community:
+                    if curr_level == 0:
+                        higher_community.append(curr_mapping[item])
+                    else:
+                        higher_community.extend(list(curr_mapping[item]))
+
+                curr_level_community = higher_community
+            finalized_clusters.append(curr_level_community)
+            
+        return finalized_clusters
+                
+    
+    def louvain(self):
+        done = False
+        initial_communities = self._singleton_partition(self.mapping)
+        mappings = list()
+        mappings.append(self.mapping)
+        adjacency_matrix = self.adjacency_matrix
+        while not done:
+            # reset cached_modularities for this level
+            self.cached_modularities = dict()
+            
+            new_communities = self._move_nodes(initial_communities, adjacency_matrix)
+            done = len(new_communities) == len(initial_communities)
+            
+            if not done:
+                new_mapping = dict()
+                for i in range(len(new_communities)):
+                    new_mapping[i] = new_communities[i]
+                mappings.append(new_mapping)
+                adjacency_matrix = self._aggregate_graph(new_communities, adjacency_matrix)
+                initial_communities = self._singleton_partition(new_mapping)
+                
+        return self._flatten_clusters(new_communities, mappings)
+            
+            
 class GargIndex:
     def __init__(self, df, key_from, key_to):
         edges = df[[key_from, key_to]]
