dce7f04bc9c8ad5b4a7987748c0760433b475527
zizoisprogramming <ziadsamerbarca@gmail.com>
2026-06-26 19:21:57 +0300

    refactor: add docker to make file for hadoop

--- StorageKernel/clients.go
+++ StorageKernel/clients.go
@@ -11,7 +11,7 @@ func newRedisClient() *redis.Client {
 	file_name = "client.go"
 	logger.Info(" - [" + file_name + "] - Creating Redis Client")
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "localhost:6379", // TODO: to be to env
 		Password: "", // no password
 		DB:       0,  // use default DB
 		Protocol: 2,
@@ -30,7 +30,7 @@ func newRedisClient() *redis.Client {
 func newHDFSClient() (*hdfs.Client, error) {
 	file_name = "client.go"
 	logger.Info(" - [" + file_name + "] - Creating HDFS Client")
-	client, err := hdfs.New("localhost:9000")
+	client, err := hdfs.New("localhost:9000") // TODO: to be to env
 	if err != nil {
 		logger.Error(" - [" + file_name + "] - " + err.Error())
 		return nil, err
