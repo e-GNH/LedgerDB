a0a9e574837f70514ccfe8f41bdc0f42593fc025
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-06-26 11:32:13 +0300

    refactor fastapi

--- aml_service/main.py
+++ aml_service/main.py
@@ -13,8 +13,10 @@ from datetime import datetime, timedelta
 from pydantic import BaseModel
 from zoneinfo import ZoneInfo
 
-from loguru import logger
+import logging
+
 BIPARTITE_GRAPH_THRESHOLD = 5
+
 class Transaction(BaseModel):
     sender: str
     receiver: str
@@ -25,7 +27,7 @@ class Transaction(BaseModel):
     
 app = FastAPI()
 
-MLLevel_instance = None
+ml_level_instance = None
 account_types = dict()
 
 @app.post("/check_transaction")
@@ -35,117 +37,116 @@ def check_transaction(transaction_data: Transaction):
         transaction["account_type"] = transaction["sender_account_type"]
         account_types[transaction["sender"]] = transaction["sender_account_type"]
         account_types[transaction["receiver"]] = transaction["receiver_account_type"]
-        logger.info(f"Received transaction: {transaction}")
-        if transaction["receiver"] == transaction["sender"]:
+        
+        if (transaction["sender"] == transaction["receiver"]):
             return {
                 "status": "rejected",
-                "reason": "Sender and receiver cannot be the same"
+                "reason": "sender and receiver can't be the same",
             }
+        
         ok, reason = level_1_check_transaction(transaction)
-
+        logging.info(f"results checking transaction {ok}, {reason}")
         if ok:
             level_2_add_transaction(transaction)
-            
-        logger.info(f"Return result from level 1 check: {(ok, reason)}")
+            return {
+                "status": "approved",
+                "reason": reason,
+            }
         
         return {
-            "status": "approved" if ok else "rejected",
-            "reason": reason
+                "status": "rejected",
+                "reason": reason,
         }
     except Exception as e:
-        logger.error(f"Error processing transaction: {e}")
+        logging.warning(f"error checking transaction {e}")
         return {
-            "status": "INTERNAL_ERROR",
-            "reason": str(e)
+                "status": "INTERNAL_ERROR",
+                "reason": str(e),
         }
 
 @app.post("/check_accounts")
 def check_accounts():
     try:
-        global MLLevel_instance
-        
+        global ml_level_instance
+        global account_types
         graph = level_2_get_graph()
         
         current_time = datetime.now(ZoneInfo("Africa/Cairo"))
-        previous_week_time = current_time - timedelta(days=7)
+        cutoff_time = current_time - timedelta(days=7)
+        graph.clean_edges(cutoff_time)
+        if len(graph.get_accounts()) == 0:
+            logging.warning(f"empty graph for check accounts")
+            return {
+            "status": "success",
+            "reason": "graph is empty",
+            "level_2": [],
+            "level_3": []
+            }
+            
+        curr_graph_accounts = set(graph.get_accounts())
+        curr_accounts_types_cached = set(account_types.keys())
         
-        graph.clean_edges(cutoff_time=previous_week_time)
+        for acc in curr_accounts_types_cached:
+            if acc not in curr_graph_accounts:
+                del account_types[acc]
+                
+        if ml_level_instance is None:
+            ml_level_instance = MLLevel(graph)
+        else:
+            ml_level_instance.update_graph(graph)
         
         level_2_results = {}
         level_3_results = {}
-        if len(graph.get_accounts()) == 0:
-            logger.warning("Graph is empty, skipping account checks")
-            return {
-                "status": "success",
-                "reason": "Graph is empty, no accounts to check",
-                "level_2": [],
-                "level_3": []   
-            }
+
         for account in graph.get_accounts():
             if account not in account_types:
-                logger.warning(f"Account {account} not found in account types mapping, skipping account level checks, defaulting to individual")
-                account_type = "individual"
-            else:
-                account_type = account_types[account]
-            result, reason = level_2_check_account(account, account_type)
-            level_2_results[account] = {"result": result, "reason": reason}
+                logging.warning(f"account {account} not in account_types defaulting to PERSON")
+                account_types[account] = "PERSON"
+            result, reason = level_2_check_account(account, account_types[account])
+            if not result:
+                ## account check failed
+                level_2_results[account] = {
+                    "reason": reason,
+                }
         
-        current_accounts = set(graph.get_accounts())
-        to_delete_types_cache = set()
-        for account in account_types.keys():
-            if account not in current_accounts:
-                to_delete_types_cache.add(account)
-        for account in to_delete_types_cache:
-            del account_types[account]
-        
-        if MLLevel_instance is None:
-            MLLevel_instance = MLLevel(graph)
-        else:
-            MLLevel_instance.update_graph(graph)
-        level_3_results = MLLevel_instance.predict_all()
-        level_2_response = []
-        level_3_response = []
-
-        communities = MLLevel_instance.get_communities()
-
-        if communities is not None:
-            for community in communities:
-                subgraph_bipartite = graph.check_bipartite_subgraph(community, BIPARTITE_GRAPH_THRESHOLD)
-                if subgraph_bipartite:
-                    for account in community:
-                        if account in level_2_results and level_2_results[account]["result"]:
-                            level_2_response.append({
-                            "account": account,
-                            "reason": "Account is part of bipartite subgraph"
-                        })
-                            ## this account didn't fail
-        ## Only include accounts that failed level 2 checks in the response
-        for account in level_2_results:
-            result = level_2_results[account]
-            if not result["result"]:
-                level_2_response.append({
-                    "account": account,
-                    "reason": result["reason"]
-                })
+        level_3_results = ml_level_instance.predict_all()
+        level_3_flagged_accounts = []
         for account in level_3_results:
-            result = level_3_results[account]
-            if result[0]:  ## result[0] is the boolean indicating if the account is flagged in level 3 check
-                level_3_response.append({
+            if level_3_results[account][0]:
+                level_3_flagged_accounts.append({
                     "account": account,
-                    "score": result[1]  ## result[1] is the score from the ML model
+                    "score": level_3_results[account][1]
                 })
-
+                
+        communities =ml_level_instance.get_communities()
+        for community in communities:
+            is_bipartite = graph.check_bipartite_subgraph(community, BIPARTITE_GRAPH_THRESHOLD)
+            if is_bipartite:
+                for account in community:
+                    if account not in level_2_results:
+                        level_2_results[account] = {
+                            "reason":  "account is a part of bipartite graph"
+                            }
+        
+        level_2_flagged_accounts = list()
+        for account in level_2_results:
+            level_2_flagged_accounts.append({ 
+                                        "account":account,
+                                        "reason": level_2_results[account]["reason"]
+                                        })
         return {
             "status": "success",
             "reason": "",
-            "level_2": level_2_response,
-            "level_3": level_3_response
+            "level_2": level_2_flagged_accounts,
+            "level_3": level_3_flagged_accounts
         }
+            
     except Exception as e:
-        logger.error(f"Error processing account checks: {e}")
+        
+        logging.warning(f"error checking accounts {e}")
         return {
-            "status": "INTERNAL_ERROR",
-            "reason": str(e),
-            "level_2": [],
-            "level_3": []
+                "status": "INTERNAL_ERROR",
+                "reason": str(e),
+                "level_2": [],
+                "level_3": []
         }
